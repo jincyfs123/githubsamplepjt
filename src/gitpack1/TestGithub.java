7613245262ff86c30c6c129c16ef2ed0251fbdfc
jincy <jincyfs77@gmail.com>
2025-04-02 17:44:40 +0530

    third commit by jincy

---
+++ src/gitpack1/TestGithub.java
@@ -0,0 +1,13 @@
+package gitpack1;
+
+public class TestGithub {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		   System.out.println(" welocme to STC");
+			}
+
+	}
+
+
