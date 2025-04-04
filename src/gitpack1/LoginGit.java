8fd261b97b5c322445cf6bff6bcb79c781f68602
jincy <jincyfs77@gmail.com>
2025-04-04 10:22:50 +0530

    commit to loginbranch

--- src/gitpack1/LoginGit.java
+++ src/gitpack1/LoginGit.java
@@ -3,6 +3,9 @@ package gitpack1;
 public class LoginGit {
 
 	public static void main(String[] args) {
+		System.out.println("welcome to login page");
+		
+		
 		// TODO Auto-generated method stub
 
 	}
