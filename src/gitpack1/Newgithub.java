afdb00658cfb0749fd81d5579b2243f47bf4d34c
jincy <jincyfs77@gmail.com>
2025-04-04 09:47:15 +0530

    commit the text to branch_x

---
+++ src/gitpack1/Newgithub.java
@@ -0,0 +1,11 @@
+package gitpack1;
+
+public class Newgithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("hi...how r u");
+  
+	}
+
+}
---
+++ src/gitpack1/newdemo.java
@@ -0,0 +1,10 @@
+package gitpack1;
+
+public class newdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
